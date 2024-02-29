ba68f13fadef7fd69af12bdc0054096cafbe401c
mhmiwamiwaus <mhmiwa@miwa.us>
2024-02-29 16:13:06 -0700

    Created the TaskForm view, and added it to the Controller for testing purposes.  Currently the form is commented out because it won't compile without the non-existing model references.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Mission_8.Controllers
             return View();
         }
 
+        public IActionResult TaskForm()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
