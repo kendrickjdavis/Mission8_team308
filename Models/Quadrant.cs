9b99522c73178fa3808aedc64bf317537808fd61
ekaznick <ethankasunick@gmail.com>
2024-02-29 22:24:54 -0700

    Added models for database

---
+++ Models/Quadrant.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission_8.Models
+{
+    public class Quadrant
+    {
+        [Key]
+        public int QuadrantId { get; set; }
+        public int QuadrantNumber { get; set; }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mission_8.Models
+{
+    public class Task
+    {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+        [Required]
+        public string TaskDescription { get; set; }
+        public string DueDate { get; set; }
+        [Required]
+        [ForeignKey("CategoryId")]
+        public int CategoryId { get; set; }
+        [Required]
+        [ForeignKey("QuadrantId")]
+        public int QuadrantId { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
