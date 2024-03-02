125964a1e44e34715ad4472b129a062415d307d5
mhideomiwa <mhmiwa@miwa.us>
2024-03-01 23:45:04 -0700

    More fixes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace Mission_8.Controllers
 
         public IActionResult Privacy()
         {
-            return View();
+            return Redirect("https://nsa.gov");
         }
 
         [HttpGet]
